3e9cba1486cb899a021c3120fb2ddb486c2e79c3
Vaibhav Agnihotri <vaibhavagnihotri@outlook.in>
2022-01-10 04:04:18 +0530

    Ordering Fix

--- src/types/block_response.rs
+++ src/types/block_response.rs
@@ -1,39 +1,56 @@
-use serde::Serialize;
+use serde::ser::SerializeMap;
+use serde::{Serialize, Serializer};
 use std::collections::HashMap;
 use web3::types::U64;
 use web3::types::{BlockId, BlockNumber, H2048, H256};
 
-#[derive(Debug, Serialize)]
-pub struct BlockResponse(HashMap<U64, Block>);
+#[derive(Debug)]
+pub struct BlockResponse(Vec<Block>);
 
 impl BlockResponse {
     pub fn add(&mut self, block: &web3::types::Block<H256>) {
-        match block.number {
-            Some(ref number) => {
-                self.0.insert(
-                    number.clone(),
-                    Block {
-                        hash: block.hash,
-                        logs_bloom: block.logs_bloom,
-                    },
-                );
-            }
-            None => {
-                //Pending Block
-            }
+        if let Some(number) = block.number {
+            self.0.push(Block {
+                hash: block.hash,
+                logs_bloom: block.logs_bloom,
+                number,
+            })
+        } else {
+            //Skip pending blocks
         }
     }
 
     pub fn new() -> Self {
-        BlockResponse(HashMap::new())
+        BlockResponse(vec![])
     }
 }
 
-#[derive(Debug, Serialize)]
+impl Serialize for BlockResponse {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut s = serializer.serialize_map(Some(self.0.len()))?;
+        let mut block_list = self.0.clone();
+        block_list.sort_by(|a, b| b.number.cmp(&a.number));
+
+        for block in block_list {
+            s.serialize_entry(&format!("{:x}", block.number), &block);
+        }
+        s.end()
+    }
+}
+
+#[derive(Debug, Clone, Serialize)]
 struct Block {
     /// Hash of the block
     pub hash: Option<H256>,
     /// Logs bloom
     #[serde(rename = "logsBloom")]
     pub logs_bloom: Option<H2048>,
+    /// Block number
+    /// Skipping serialization of this
+    /// because not required in response
+    #[serde(skip)]
+    pub number: U64,
 }
